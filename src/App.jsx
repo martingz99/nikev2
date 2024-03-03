2a528f24bff66815d6b60fa3464def621547e91a
martin <martudev@gmail.com>
2024-03-03 19:50:36 -0300

    version2

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,19 @@ import './App.css'
 
 function App() {
 
+  const saludo = "Tienda de ropa Nike"
+
+  const compraste = ( ) => {
+    alert("Compraste la prenda!")
+  }
+
   return (
     <>
-      <div>
-          <h1>Proyecto de ropa </h1>
+      <div className='title'>
+        <img src="/img/nike.jpg" alt="" />
+        <p>dsajdlkhsa</p>
+        <h1>{saludo} </h1>
+        <button onClick={compraste}>Comprar</button>
       </div>
     </>
   )
